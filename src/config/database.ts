be9a3d97dc2b9ecd3d1946f5e0be3b01000a4880
Lucas Junker Narais <lucasnarvais@gmail.com>
2024-10-01 18:11:28 -0300

    Ajustes no retorno das funcoes de login/cadastro

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,7 +4,7 @@ const pool = createPool({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: '1234',
+  password: 'Db3264128jn!',
   database: 'Sivt',
   waitForConnections: true,
   connectionLimit: 10,
---
+++ src/types/authTypes.ts
@@ -0,0 +1,6 @@
+export type Auth = {
+    name: string,
+    role: string,
+    id: number,
+    token: string
+}
