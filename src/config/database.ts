8e4dc73f54ab47c3ecf0ea4183ebf0b8bedcdfe7
Lucas Junker Narais <lucasnarvais@gmail.com>
2024-11-29 16:37:47 -0300

    update msql confiog

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,11 +1,11 @@
 import { createPool } from 'mysql2/promise';
 
 const pool = createPool({
-  host: 'localhost',
+  host: '127.0.0.1',
   port: 3306,
   user: 'root',
-  password: 'Db3264128jn!',
-  database: 'Sivt',
+  password: '12345',
+  database: 'sivt',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
